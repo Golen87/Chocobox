60f94d58f16977b722217975b536e753f8b18b32
Golen <golen87@hotmail.com>
2024-12-03 18:31:23 +0100

    Remove unneeded files

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,11 +1,9 @@
 import { BaseScene } from "@/scenes/BaseScene";
 import { Player } from "@/components/Player";
-import { UI } from "@/components/UI";
 
 export class GameScene extends BaseScene {
 	private background: Phaser.GameObjects.Image;
 	private player: Player;
-	private ui: UI;
 
 	constructor() {
 		super({ key: "GameScene" });
@@ -23,15 +21,12 @@ export class GameScene extends BaseScene {
 			this.player.doABarrelRoll();
 		});
 
-		this.ui = new UI(this);
-
 		this.initTouchControls();
 	}
 
 	update(time: number, delta: number) {
 		this.player.update(time, delta);
 	}
-
 
 	initTouchControls() {
 		this.input.addPointer(2);
